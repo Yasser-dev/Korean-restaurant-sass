0c0e249893e7994a09c9cab6bdf6a199ca9b6d14
Yasser <yehab2000@gmail.com>
2020-06-15 19:52:27 +0800

    Scroll Navigation animation

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".food_header .overlay .navbar a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
